bcaf590b9794d2afceea492c42f6b17ebe1e7ff5
jhell85 <jhell85@gmail.com>
2020-07-26 12:15:15 -0700

    add axios request to get imgs from Unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID 7kckQHFGjVG9L0_8k4jzeFrvhjsOF2bkz3Hxiv36Tw0",
+      },
+    });
+  }
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,23 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
+  // another way to solve issues of event handler not binding 'this' from functions:
+  // Error would look something like ... TypeError: Cannot read property 'props' of undefined
+  //  use a fat arrow function, pass in the event as an argument when passing the function as a prop down like in the onChange function for the input field
+  // OR ANOTHER WAY
+  // constructor(){
+  //   this.term = this.term .bind(this);
+  // }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
             <input
